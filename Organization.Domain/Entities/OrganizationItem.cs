a437d1e7a5911319ee5fcd61f88a26469c0f7a3a
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-10-28 14:53:54 -0300

    implementando metodos 'marcar como concluido', 'desmarcar' e 'atualizar o titulo'

--- Organization.Domain/Entities/OrganizationItem.cs
+++ Organization.Domain/Entities/OrganizationItem.cs
@@ -4,10 +4,32 @@ namespace Organization.Domain.Entities
 {
     public class OrganizationItem : Entity
     {
+        public OrganizationItem(string title, string user, DateTime date)
+        {
+            Title = title;
+            Done = false;
+            //false pq a tarefa nunca começa ja concluida
+            Date = date;
+            User = user;
+        }
+
         public string Title { get; private set; }
         public bool Done { get; private set; }
         public DateTime Date { get; private set; }
         public string User { get; private set; }
         //referencia do usuario
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+        public void MarkAsUnDone()
+        {
+            Done = false;
+        }
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
     }
 }
