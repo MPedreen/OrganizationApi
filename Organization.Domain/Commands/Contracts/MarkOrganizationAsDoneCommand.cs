83c7bb549b46a39d3e340a64a5dc72d2e5b8547b
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-10-29 21:45:27 -0300

    criando contrato: marcar tarefa como concluida

---
+++ Organization.Domain/Commands/Contracts/MarkOrganizationAsDoneCommand.cs
@@ -0,0 +1,24 @@
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Organization.Domain.Commands.Contracts
+{
+    public class MarkOrganizationAsDoneCommand : Notifiable, ICommand
+    {
+        public MarkOrganizationAsDoneCommand() { }
+
+        public MarkOrganizationAsDoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
