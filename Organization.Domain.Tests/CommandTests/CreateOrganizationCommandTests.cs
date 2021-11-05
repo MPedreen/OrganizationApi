f40a1f3c1a495c4e4a91c08e2a9fb670e69bff1f
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-05 20:10:56 -0300

    refatorando os testes, validando no metodo construtor

--- Organization.Domain.Tests/CommandTests/CreateOrganizationCommandTests.cs
+++ Organization.Domain.Tests/CommandTests/CreateOrganizationCommandTests.cs
@@ -10,20 +10,24 @@ namespace Organization.Domain.Tests.CommandTests
         private readonly CreateOrganizationCommand _invalidCommand = new CreateOrganizationCommand("", "", DateTime.Now);
         private readonly CreateOrganizationCommand _validCommand = new CreateOrganizationCommand("Titulo da tarefa", "pedroferreira", DateTime.Now);
 
+        public CreateOrganizationCommandTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido()
         {
             var command = new CreateOrganizationCommand("", "", DateTime.Now);
-            command.Validate();
-            Assert.AreEqual(command.Valid, false);
+            Assert.AreEqual(_invalidCommand.Valid, false);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido()
         {
             var command = new CreateOrganizationCommand("Titulo da Tarefa", "Pedro Ferreira", DateTime.Now);
-            command.Validate();
-            Assert.AreEqual(command.Valid, true);
+            Assert.AreEqual(_validCommand.Valid, true);
         }
     }
 }
