23d72f79b609d61f3f462d7ae201bd7395a81d15
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-05 23:45:46 -0300

    forcando o teste a falhar

--- Organization.Domain.Tests/HandlerTests/CreateOrganizationHandlerTests.cs
+++ Organization.Domain.Tests/HandlerTests/CreateOrganizationHandlerTests.cs
@@ -5,7 +5,14 @@ namespace Organization.Domain.Tests.HandlerTests
     [TestClass]
     public class CreateOrganizationHandlerTests
     {
-        public void UnitTest()
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
         {
             Assert.Fail();
         }
