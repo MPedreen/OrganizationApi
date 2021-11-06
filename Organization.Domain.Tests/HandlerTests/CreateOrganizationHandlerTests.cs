aa152285ea21e405cfaec3ad18ebde7b0cf3271f
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-05 23:41:49 -0300

    iniciando os testes dos handlers

---
+++ Organization.Domain.Tests/HandlerTests/CreateOrganizationHandlerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Organization.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateOrganizationHandlerTests
+    {
+        public void UnitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
