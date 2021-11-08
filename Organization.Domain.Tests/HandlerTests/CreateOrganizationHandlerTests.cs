dd10356e50e14a71c2978b261ab67b55668afef1
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-08 16:17:55 -0300

    refatorando o codigo do handler

--- Organization.Domain.Tests/HandlerTests/CreateOrganizationHandlerTests.cs
+++ Organization.Domain.Tests/HandlerTests/CreateOrganizationHandlerTests.cs
@@ -10,22 +10,28 @@ namespace Organization.Domain.Tests.HandlerTests
     [TestClass]
     public class CreateOrganizationHandlerTests
     {
+        private readonly CreateOrganizationCommand _invalidCommand = new CreateOrganizationCommand("", "", DateTime.Now);
+        private readonly CreateOrganizationCommand _validCommand = new CreateOrganizationCommand("Titulo da tarefa", "pedroferreira", DateTime.Now);
+        private readonly OrganizationHandler _handler = new OrganizationHandler(new FakeOrganizationRepository());
+        private GenericCommandResult _result = new GenericCommandResult();
+
+        public CreateOrganizationHandlerTests()
+        {
+
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido_deve_interromper_a_execucao()
         {
-            var command = new CreateOrganizationCommand("", "", DateTime.Now);
-            var handler = new OrganizationHandler(new FakeOrganizationRepository());
-            var result = (GenericCommandResult)handler.Handle(command);
-            Assert.AreEqual(result.Success, false);
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Success, false);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido_deve_criar_a_tarefa()
         {
-            var command = new CreateOrganizationCommand("Título da Tarefa", "pedroferreira", DateTime.Now);
-            var handler = new OrganizationHandler(new FakeOrganizationRepository());
-            var result = (GenericCommandResult)handler.Handle(command);
-            Assert.AreEqual(result.Success, true);
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Success, true);
         }
     }
 }
