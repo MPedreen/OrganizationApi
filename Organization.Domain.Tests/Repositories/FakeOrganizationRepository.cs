57e7f7fc644336f237797d013353efdfe5599803
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-08 15:44:46 -0300

    Fake Repository para testes

---
+++ Organization.Domain.Tests/Repositories/FakeOrganizationRepository.cs
@@ -0,0 +1,18 @@
+using Organization.Domain.Entities;
+using Organization.Domain.Repositories;
+
+namespace Organization.Domain.Tests.Repository
+{
+    public class FakeOrganizationRepository : IOrganizationRepository
+    {
+        public void Create(OrganizationItem organization)
+        {
+
+        }
+
+        public void Update(OrganizationItem organization)
+        {
+
+        }
+    }
+}
