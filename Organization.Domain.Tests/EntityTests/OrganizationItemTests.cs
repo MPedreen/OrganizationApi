c400b79339fbdf25fc1a0a6a642c1cc8b132cb98
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-08 16:37:55 -0300

    iniciando testes da entidade

---
+++ Organization.Domain.Tests/EntityTests/OrganizationItemTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Organization.Domain.Tests.EntityTests
+{
+    public class OrganizationItemTests
+    {
+        [TestMethod]
+        public void Dado_um_novo_organization_o_mesmo_nao_pode_ser_concluido()
+        {
+            Assert.Fail();
+        }
+    }
+}
