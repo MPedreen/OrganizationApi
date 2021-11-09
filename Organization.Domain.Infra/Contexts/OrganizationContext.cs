fee616d7092981e3d89213f6990d7f5b0a1cb87b
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-08 22:59:40 -0300

    finalizando context e dbset

--- Organization.Domain.Infra/Contexts/OrganizationContext.cs
+++ Organization.Domain.Infra/Contexts/OrganizationContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using Organization.Domain.Entities;
 
 namespace Organization.Domain.Infra.Contexts
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<OrganizationItem> Organizations { get; set; }
     }
 }
