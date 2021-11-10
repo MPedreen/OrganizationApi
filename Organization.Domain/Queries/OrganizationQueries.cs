acb7550e49d98b89cdefffd74719c580fa2ace94
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-10 19:22:07 -0300

    queries = metodo getbyperiod

--- Organization.Domain/Queries/OrganizationQueries.cs
+++ Organization.Domain/Queries/OrganizationQueries.cs
@@ -10,15 +10,25 @@ namespace Organization.Domain.Queries
         {
             return x => x.User == user;
         }
+
         public static Expression<Func<OrganizationItem, bool>> GetAllDone(string user)
         {
             return x => x.User == user && x.Done;
             //ou
             //return x => x.User == user && x.Done == true;
         }
+
         public static Expression<Func<OrganizationItem, bool>> GetAllUnDone(string user)
         {
             return x => x.User == user && x.Done == false;
+        }
+
+        public static Expression<Func<OrganizationItem, bool>> GetByPeriod(string user, DateTime date, bool done)
+        {
+            return x =>
+                x.User == user &&
+                x.Done == done &&
+                x.Date.Date == date.Date;
         }
     }
 }
