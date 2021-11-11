7f5b34e50d601b8a30e09836485268884d8f1332
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-10 23:31:01 -0300

    finalizando estrutura do repositorio

--- Organization.Domain.Infra/Repositories/OrganizationRepository.cs
+++ Organization.Domain.Infra/Repositories/OrganizationRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Organization.Domain.Entities;
 using Organization.Domain.Repositories;
 
@@ -11,7 +12,27 @@ namespace Organization.Domain.Infra.Repositories
             throw new NotImplementedException();
         }
 
+        public IEnumerable<OrganizationItem> GetAll(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OrganizationItem> GetAllDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OrganizationItem> GetAllUndone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
         public OrganizationItem GetById(Guid id, string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OrganizationItem> GetByPeriod(string user, DateTime date, bool done)
         {
             throw new NotImplementedException();
         }
