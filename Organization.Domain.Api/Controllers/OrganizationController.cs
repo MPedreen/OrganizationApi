170972bdecfc29087390f6fec07bf5d960859d9b
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-11 22:46:31 -0300

    criando organizationcontroller, definindo que é uma controllerapi e setando rota (v1/organizations, vai cair sempre no organizationcontroller)

---
+++ Organization.Domain.Api/Controllers/OrganizationController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Organization.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/organizations")]
+    public class OrganizationController : ControllerBase
+    {
+
+    }
+}
