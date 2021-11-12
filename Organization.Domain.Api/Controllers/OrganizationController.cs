aeecbfd72c1710df41777cf4651b37055f0bdf1b
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-11 22:52:34 -0300

    recebendo um command direto do corpo da requisiçao (convertendo json para command)

--- Organization.Domain.Api/Controllers/OrganizationController.cs
+++ Organization.Domain.Api/Controllers/OrganizationController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Organization.Domain.Commands;
+using Organization.Domain.Commands.Contracts;
 
 namespace Organization.Domain.Api.Controllers
 {
@@ -6,6 +8,11 @@ namespace Organization.Domain.Api.Controllers
     [Route("v1/organizations")]
     public class OrganizationController : ControllerBase
     {
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateOrganizationCommand command)
+        {
 
+        }
     }
 }
