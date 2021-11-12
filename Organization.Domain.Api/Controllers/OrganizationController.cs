fdf911f5790d336efc70610308066b410ea6ee4d
Pedro Ferreira <pedrophpvp12@gmail.com>
2021-11-12 15:47:16 -0300

    finalizando metodo get no controller, testando com postman e tudo ok

--- Organization.Domain.Api/Controllers/OrganizationController.cs
+++ Organization.Domain.Api/Controllers/OrganizationController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Organization.Domain.Commands;
 using Organization.Domain.Commands.Contracts;
+using Organization.Domain.Entities;
 using Organization.Domain.Handlers;
+using Organization.Domain.Repositories;
 
 namespace Organization.Domain.Api.Controllers
 {
@@ -9,6 +12,13 @@ namespace Organization.Domain.Api.Controllers
     [Route("v1/organizations")]
     public class OrganizationController : ControllerBase
     {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<OrganizationItem> GetAll([FromServices] IOrganizationRepository repository)
+        {
+            return repository.GetAll("pedroferreira");
+        }
+
         [Route("")]
         [HttpPost]
         public GenericCommandResult Create([FromBody] CreateOrganizationCommand command, [FromServices] OrganizationHandler handler)
